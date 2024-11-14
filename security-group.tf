27f6cbb6f86958640ef5987aa5c7bbd4a8aca10d
PolycarpeSI <psiekambe@gmail.com>
2024-11-14 23:45:14 +0100

    from aws_security_group.sg2.name to aws_security_group.sg1.name

--- security-group.tf
+++ security-group.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "sg1" {
         to_port = 80
         protocol = "tcp"
         #cidr_blocks = ["0.0.0.0/0"] # This sg is lock for direct internet traffic
-        security_groups = [ aws_security_group.sg2.name ] # but can be access throw the the sg having the name (aws_security_group.sg2.name), so the elb (Load balancer)
+        security_groups = [ aws_security_group.sg1.name ] # but can be access throw the the sg having the name (aws_security_group.sg2.name), so the elb (Load balancer)
     }
 
     egress {
